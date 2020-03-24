0a4ba52843295f922cdacb7a6420e3a8a9df9906
Nasrul Hazim <nasrulhazim.m@gmail.com>
2020-03-25 03:09:55 +0800

    Added uuid() and validUuid() helpers

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -1,1 +1,28 @@
 <?php
+
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+
+if (! function_exists('uuid')) {
+    /**
+     * Generate uuid.
+     */
+    function uuid(): string
+    {
+        return (string) Str::uuid();
+    }
+}
+
+if (! function_exists('validUuid')) {
+    /**
+     * Validate given uuid.
+     */
+    function validUuid(string $value): bool
+    {
+        return ! Validator::make([
+                'uuid' => $value,
+            ], [
+            'uuid' => 'string|uuid',
+        ])->fails();
+    }
+}
